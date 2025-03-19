4300c7d2f5a49be89ffbfd612a5bcaf561d802f2
Tor-Skaarva <torskaa@gmail.com>
2025-03-19 10:43:48 -0400

    Add files via upload
    Realized I forgot to submit this.

---
+++ code.js
@@ -0,0 +1,11 @@
+// I will write a code assigning integers to variables and then having the combination display as an alert message.
+console.log("Hello");
+//First combo
+let a = 5 + 5;
+//Second combo
+let b = 60 - 20;
+//Third combo
+let c = 2 * 19.5;
+let message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+alert(message + " " + a + "-" + b + "-" + c);
